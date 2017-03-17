9218696acf3af39ee76e4da34a1145bdb7b0d166
jermbo <jermbo@cosmicstrawberry.com>
2017-03-17 16:54:38 -0400

    setting up dev env for meetup

---
+++ RESTful-CRUD/gulp.config.js
@@ -0,0 +1,50 @@
+const yargs = require('yargs').argv;
+
+module.exports = () => {
+    const sourcePath = './src',
+        deployPath = './build',
+        config = {
+            sourcePath: sourcePath,
+            deployPath: deployPath,
+            url: (yargs.url) ? yargs.url : deployPath,
+            styles: {
+                source: `${sourcePath}/sass/**/*.{scss, sass}`,
+                build: `${deployPath}/styles`
+            },
+            html: {
+                source: `${sourcePath}/**/*.jade`,
+                build: `${deployPath}/`
+            },
+            script: {
+                source: `${sourcePath}/scripts/**/*.js`,
+                build: `${deployPath}/scripts`
+            },
+            images: {
+                source: `${sourcePath}/images/**/*.*`,
+                build: `${deployPath}/images`
+            },
+            options:{
+                api: {
+                    port: 8000
+                },
+                browser: {
+                    // proxy: config.url,
+                    server: {
+                        baseDir: './build',
+                    },
+                    port: 4000,
+                    ghostMode: {
+                        clicks: true,
+                        location: true,
+                        forms: true,
+                        scroll: true
+                    },
+                    injectChanges: true,
+                    notify: true,
+                    reloadDelay: 0
+                }
+            }
+        };
+
+    return config;
+}
